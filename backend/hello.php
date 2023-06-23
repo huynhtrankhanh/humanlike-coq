ab53c4f53714af83b655b757e6bfe7470221aec9
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2023-06-23 14:45:42 +0000

    Use openai-php/client

--- backend/hello.php
+++ backend/hello.php
@@ -9,59 +9,12 @@ $dotenv->load();
 
 $openai_api_key = $_ENV['OPENAI_API_KEY'];
 
-$client = new Client([
-    'base_uri' => 'https://api.openai.com',
-    'headers' => [
-        'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer ' . $openai_api_key
-    ]
+$client = OpenAI::client($openai_api_key);
+
+$result = $client->completions()->create([
+    'model' => 'text-davinci-003',
+    'prompt' => 'PHP is',
 ]);
 
-function JQN($x) {
-    return $x * $x + 3;
-}
-
-$data = [
-    'model' => 'gpt-3.5-turbo-0613',
-    'messages' => [
-        [
-            'role' => 'user',
-            'content' => 'What is the result of JQN(35)?'
-        ]
-    ],
-    'functions' => [
-        [
-            'name' => 'JQN',
-            'description' => 'Calculate the result of JQN function',
-            'parameters' => [
-                'type' => 'object',
-                'properties' => [
-                    'x' => [
-                        'type' => 'integer',
-                        'description' => 'The input value for the JQN function'
-                    ]
-                ],
-                'required' => ['x']
-            ]
-        ]
-    ],
-    'temperature' => 0.7
-];
-
-$response = $client->post('/v1/chat/completions', [
-    'json' => $data
-]);
-
-$result = json_decode($response->getBody(), true);
-
-if (isset($result['choices'][0]['message']['function_call'])) {
-    $function_call = $result['choices'][0]['message']['function_call'];
-    if ($function_call['name'] == 'JQN') {
-        $arguments = json_decode($function_call['arguments'], true);
-        $jqn_result = JQN($arguments['x']);
-        $result['choices'][0]['message']['content'] = "The result of JQN({$arguments['x']}) is {$jqn_result}.";
-    }
-}
-
-print_r($result); // Or use echo json_encode($result, JSON_PRETTY_PRINT);
+echo $result['choices'][0]['text'];
 ?>
