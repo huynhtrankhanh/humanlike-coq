81e579902cd7b1568c8e55177e079790d3020425
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-06-30 20:49:12 +0700

    Serialize messages

--- backend/conversation.php
+++ backend/conversation.php
@@ -1,8 +1,11 @@
 <?php
-
 declare(strict_types=1);
 
 abstract class Message {
+    abstract function toArray(): array;
+}
+
+class UserMessage extends Message {
     private string $content;
 
     public function __construct(string $content) {
@@ -12,33 +15,70 @@ abstract class Message {
     public function getContent(): string {
         return $this->content;
     }
+
+    public function toArray(): array {
+        return [
+            'role' => 'user',
+            'content' => $this->getContent()
+        ];
+    }
 }
 
-class UserMessage extends Message {}
-
 class AssistantMessage extends Message {
+    private ?string $content;
     private ?FunctionCall $functionCall;
 
-    public function __construct(string $content, ?FunctionCall $functionCall = null) {
-        parent::__construct($content);
+    public function __construct(?string $content, ?FunctionCall $functionCall = null) {
+        $this->content = $content;
         $this->functionCall = $functionCall;
+    }
+
+    public function getContent(): ?string {
+        return $this->content;
     }
 
     public function getFunctionCall(): ?FunctionCall {
         return $this->functionCall;
     }
+
+    public function toArray(): array {
+        $arr = [ 'role' => 'assistant' ];
+
+        if ($this->getContent() !== null) {
+            $arr['content'] = $this->getContent();
+        }
+
+        if ($this->getFunctionCall() !== null) {
+            $arr['function_call'] = $this->getFunctionCall()->toArray();
+        }
+
+        return $arr;
+    }
 }
 
 class FunctionMessage extends Message {
     private string $name;
+    private string $content;
 
     public function __construct(string $name, string $content) {
-        parent::__construct($content);
         $this->name = $name;
+        $this->content = $content;
     }
 
     public function getName(): string {
         return $this->name;
+    }
+
+    public function getContent(): string {
+        return $this->content;
+    }
+
+    public function toArray(): array {
+        return [
+            'role' => 'function',
+            'name' => $this->getName(),
+            'content' => $this->getContent()
+        ];
     }
 }
 
@@ -54,6 +94,25 @@ class Conversation {
     }
 
     public function getMessages(): array {
-        return $this->messages;
+        return array_map(function ($message) {
+            return $message->toArray();
+        }, $this->messages);
+    }
+}
+
+class FunctionCall {
+    private string $name;
+    private string $arguments;
+
+    public function __construct(string $name, string $arguments) {
+        $this->name = $name;
+        $this->arguments = $arguments;
+    }
+
+    public function toArray(): array {
+        return [
+            'name' => $this->name,
+            'arguments' => $this->arguments
+        ];
     }
 }
