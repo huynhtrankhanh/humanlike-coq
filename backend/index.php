6533d3701c26d08e6883d191003b27a309726e53
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-07-02 15:31:08 +0000

    Add small demo

--- backend/index.php
+++ backend/index.php
@@ -13,20 +13,32 @@ $dotenv->load();
 $openai_api_key = $_ENV["OPENAI_API_KEY"];
 
 $client = new OpenAIClientManager(
-    $openai_api_key,
-    [
-        new FunctionDefinition(
-            "displayCongratulationsAnimation",
-            "Displays an eye catching congratulations animation to the user",
-            []
-        ),
-    ],
-    "gpt-4-32k"
+    $openai_api_key
 );
 
 $conversation = new Conversation();
-$conversation->addMessage(new UserMessage("display congrats animation please"));
+$conversation->addMessage(new UserMessage("whats the weather today in bermuda"));
 
-$client->performConversation($conversation);
+$client->performConversation($conversation
+,	    [
+ new FunctionDefinition(
+    "get_current_weather",
+    "Get the current weather in a given location",
+    [
+        new ParameterDefinition(
+            "location",
+            "string",
+            "The city and state, e.g. San Francisco, CA"
+        ),
+        new ParameterDefinition(
+            "unit",
+            "string",
+            "The unit of measurement for temperature, either 'celsius' or 'fahrenheit'"
+        )
+    ]
+)    ],
+    "gpt-4-32k"
+
+);
 
 print_r($conversation->toArray());
