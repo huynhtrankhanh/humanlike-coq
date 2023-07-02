d0ed46870a84cf0127c88a0b76bf48374ec624d9
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-07-02 22:01:11 +0700

    Add function definitions

--- backend/client_manager.php
+++ backend/client_manager.php
@@ -4,27 +4,38 @@ class OpenAIClientManager
 {
     private $client;
 
-    public function __construct($apiKey)
+    public function __construct(string $apiKey)
     {
         $this->client = OpenAI::client($apiKey);
     }
 
     public function performConversation(
         Conversation $conversation,
+        array $functions = [],
         string $model = "gpt-3.5-turbo"
     ): void {
         $messages = $conversation->toArray();
 
-        // Make call to OpenAI chat service
-        $response = $this->client->chat()->create([
+        $functionList = [];
+        foreach($functions as $function) {
+          if ($function instanceof FunctionDefinition) {
+            $functionList[] = $function->getDefinition();
+          }
+        }
+
+        $payload = [
             "model" => $model,
             "messages" => $messages,
-        ]);
+        ];
 
-        // Select the first response choice
+        if (!empty($functionList)) {
+            $payload["functions"] = $functionList;
+        }
+
+        $response = $this->client->chat()->create($payload);
+
         $result = $response->choices[0];
 
-        // Create a new AssistantMessage
         $assistantMessage = new AssistantMessage(
             $result->message->content,
             isset($result->message->functionCall)
@@ -35,7 +46,55 @@ class OpenAIClientManager
                 : null
         );
 
-        // Add final message to the conversation
         $conversation->addMessage($assistantMessage);
+    }
+}
+
+class Parameter
+{
+    private string $type; // type of the parameter
+    private string $description; // description of the parameter
+
+    public function __construct(string $type, string $description)
+    {
+        $this->type = $type;
+        $this->description = $description;
+    }
+
+    public function getParameter(): array
+    {
+        return [
+            "type" => $this->type,
+            "description" => $this->description
+        ];
+    }
+}
+
+class FunctionDefinition
+{
+    private string $name; // Name of the function
+    private string $description; // Description of the function
+    private array $parameters; // Array of parameters required by function
+
+    public function __construct(string $name, string $description, array $parameters = [])
+    {
+        $this->name = $name;
+        $this->description = $description;
+
+        $this->parameters = [];
+        foreach ($parameters as $parameter) {
+            if ($parameter instanceof Parameter) {
+                $this->parameters[] = $parameter->getParameter();
+            }
+        }
+    }
+
+    public function getDefinition(): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'parameters' => $this->parameters
+        ];
     }
 }
