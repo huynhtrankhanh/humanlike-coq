53c0d7d49f8729cd7e947ecbfb8a76823a0221bb
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-07-03 08:56:58 +0700

    Use the new function handling classes

--- backend/index.php
+++ backend/index.php
@@ -4,57 +4,35 @@ declare(strict_types=1);
 require "vendor/autoload.php";
 require_once "conversation.php";
 require_once "client_manager.php";
+require_once "function_handler.php";
 
 use GuzzleHttp\Client;
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
 $openai_api_key = $_ENV["OPENAI_API_KEY"];
-
 $client = new OpenAIClientManager($openai_api_key);
+$functionHandler = new FunctionHandler();
 
 $conversation = new Conversation();
 $conversation->addMessage(
     new UserMessage("bake me a cake and display a congratulatory message")
 );
-
 $functions = [
-    new FunctionDefinition(
-        "displayCongratulatoryDialog",
-        "Display a congratulatory message. If none is supplied, just display 'CONGRATULATIONS'.",
-        [new ParameterDefinition("message", "string", "the message", false)]
-    ),
+    new FunctionDefinition("displayCongratulatoryDialog", "Display a congratulatory message.", [new ParameterDefinition("message", "string", "the message", true)]),
     new FunctionDefinition("bakeCake", "Bake a cake.", []),
 ];
 
 $isLastMessageFunctionCall = true;
+
 while ($isLastMessageFunctionCall) {
     $client->performConversation($conversation, $functions, "gpt-4-32k");
-
     $lastMessageIndex = array_key_last($conversation->toArray());
     $lastMessage = $conversation->toArray()[$lastMessageIndex];
-
-    if (
-        $lastMessage["role"] === "assistant" &&
-        isset($lastMessage["function_call"])
-    ) {
-        $lastMessageFunctionCallName = $lastMessage["function_call"]["name"];
-        if ($lastMessageFunctionCallName === "displayCongratulatoryDialog") {
-            echo "Triggered congratulations.\n";
-            $congratulationsMessage = new FunctionMessage(
-                "displayCongratulatoryDialog",
-                "Congratulatory message displayed successfully."
-            );
-            $conversation->addMessage($congratulationsMessage);
-        } elseif ($lastMessageFunctionCallName === "bakeCake") {
-            echo "Triggered bake cake.\n";
-            $bakeCakeMessage = new FunctionMessage(
-                "bakeCake",
-                "Cake has been baked successfully."
-            );
-            $conversation->addMessage($bakeCakeMessage);
-        }
+    if ($lastMessage["role"] === "assistant" && isset($lastMessage["function_call"])) {
+        $lastMessageFunctionCall = new FunctionCall($lastMessage['function_call']['name'], $lastMessage['function_call']['arguments']);
+        $responseMessage = $functionHandler->handleFunction($lastMessageFunctionCall);
+        $completionMessage = new FunctionMessage($lastMessageFunctionCall->name, $responseMessage);
+        $conversation->addMessage($completionMessage);
     } else {
         $isLastMessageFunctionCall = false;
     }
